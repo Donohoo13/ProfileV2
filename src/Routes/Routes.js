36a5fad7530cf31a14978fc33cd60ae3844eeddd
Conner Donohoo <donohoo13@Conners-MacBook-Air.local>
2018-10-30 15:38:29 -0600

    adding showcase

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 
 import Home from '../Components/Home';
 import Contact from '../Components/Contact';
+import Showcase from '../Components/Showcase';
 
 export default class Routes extends Component {
     render() {
@@ -11,6 +12,7 @@ export default class Routes extends Component {
             <Fragment>
                 <Route exact path="/" component={Home} />
                 <Route path="/contact" component={Contact} />
+                <Route path="/showcase" component={Showcase} />
             </Fragment>
         );
     }
