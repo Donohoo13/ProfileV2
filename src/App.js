ae315a409ffb1a28e2d89c6b959b34b50d49a2b4
Conner Donohoo <donohoo13@Conners-MacBook-Air.local>
2018-10-30 13:29:47 -0600

    tinkering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import MainNav from './Navbar/Navbar'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Profile</h1>
+        <MainNav/>
       </div>
     );
   }
---
+++ src/Routes/Routes.js
@@ -0,0 +1,6 @@
+import React, { Component, Fragment } from "react";
+import { Routes } from "react-router-dom";
+
+import Navbar from "../Navbar/Navbar";
+
+
