4f36fe0fe8c32db5dd3dc5bd7224af440692aa2c
Conner Donohoo <donohoo13@gmail.com>
2018-12-17 15:11:46 -0700

    gradient style change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import './CSS/Home.css';
 const Home = () => (
     <div className="home">
       <header>
-        <h1>Welcome to my profile!</h1>
+        <h1 id="title">Welcome to my profile!</h1>
       </header>
     </div>
 );
